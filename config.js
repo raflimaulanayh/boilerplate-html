74252d31e25cd1ce56380fc778307ab8f7680c37
raflimaulanayh <personal.rafly@gmail.com>
2023-08-30 06:14:33 +0700

    feat: add page dashboad status

---
+++ config.js
@@ -0,0 +1,22 @@
+// host = '172.16.153.122';	// hostname or IP address
+var host = "b37.mqtt.one"; // hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+var port = 8083;
+var topic = "1260hu2445/nesas"; // topic to subscribe to
+var useTLS = false;
+var username = "1260hu2445";
+var password = "670ehijopy";
+// username = "jjolie";
+// password = "aa";
+// topic = '1260hu2445/';		// topic to subscribe to
+// useTLS = false;
+var username = "1260hu2445";
+var password = "670ehijopy";
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        // You may customize your own custom color here
+        primary: "#3B82F6",
+        secondary: "#1D4ED8",
+        tertiary: "#CFF0FF",
+        black: "#000",
+      },
+    },
   },
   plugins: [],
 };
